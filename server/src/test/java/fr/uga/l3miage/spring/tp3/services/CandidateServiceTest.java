dbfbb76224d1544310ce0bd1ff15e00253179bcb
l3miage-seyfa <alireza.seyf@etu.univ-grenoble-alpes.fr>
2024-04-07 23:56:48 +0200

    TP VA 3

--- server/src/test/java/fr/uga/l3miage/spring/tp3/services/CandidateServiceTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/services/CandidateServiceTest.java
@@ -1,4 +1,42 @@
 package fr.uga.l3miage.spring.tp3.services;
 
+import fr.uga.l3miage.spring.tp3.components.CandidateComponent;
+import fr.uga.l3miage.spring.tp3.exceptions.rest.CandidateNotFoundRestException;
+import fr.uga.l3miage.spring.tp3.exceptions.technical.CandidateNotFoundException;
+import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
+import fr.uga.l3miage.spring.tp3.models.CandidateEvaluationGridEntity;
+import fr.uga.l3miage.spring.tp3.models.ExamEntity;
+import fr.uga.l3miage.spring.tp3.repositories.CandidateEvaluationGridRepository;
+import fr.uga.l3miage.spring.tp3.services.CandidateService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import java.util.Set;
+
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class CandidateServiceTest {
+    @Autowired
+    private CandidateService candidateService;
+
+    @MockBean
+    private CandidateEvaluationGridRepository candidateEvaluationGridRepository;
+    
+    @MockBean
+    private CandidateComponent candidateComponent;
+
+
+    @Test
+    void testgetCandidateAverageNotFound() throws CandidateNotFoundException {
+        when(candidateComponent.getCandidatById(anyLong())).thenThrow(CandidateNotFoundException.class);
+        assertThrows(CandidateNotFoundRestException.class, () -> candidateService.getCandidateAverage(1L));
+    }
 }
+
