25c00a89e14d305a9003427002e6218a4b25fb6a
l3miage-seyfa <alireza.seyf@etu.univ-grenoble-alpes.fr>
2024-04-07 23:50:38 +0200

    TP VA 2

--- server/src/test/java/fr/uga/l3miage/spring/tp3/controllers/CandidateControllerTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/controllers/CandidateControllerTest.java
@@ -1,6 +1,5 @@
 package fr.uga.l3miage.spring.tp3.controllers;
 
-import fr.uga.l3miage.spring.tp3.components.CandidateComponent;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
 import fr.uga.l3miage.spring.tp3.models.CandidateEvaluationGridEntity;
 import fr.uga.l3miage.spring.tp3.models.ExamEntity;
@@ -31,5 +30,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect")
 
 public class CandidateControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private CandidateEvaluationGridRepository candidateEvaluationGridRepository;
+
+
+    @AfterEach
+    public void clear() {
+        candidateRepository.deleteAll();
+    }
+
+    @Test
+    void getCandidateAverageDontThrow(){
+        final HttpHeaders headers = new HttpHeaders();
+        final HashMap<String,Long> urlParam = new HashMap<>();
+
+
+        CandidateEntity candidateEntity = CandidateEntity
+                .builder()
+                .id(1L)
+                .email("")
+                .build();
+        ExamEntity examEntity = ExamEntity
+                .builder()
+                .weight(1)
+                .build();
+        CandidateEvaluationGridEntity candidateEvaluationGridEntityExam = CandidateEvaluationGridEntity
+                .builder()
+                .grade(10)
+                .build();
+
+
+        candidateEvaluationGridEntityExam.setExamEntity(examEntity);
+        candidateEntity.setCandidateEvaluationGridEntities(Set.of(candidateEvaluationGridEntityExam));
+        candidateRepository.save(candidateEntity);
+
+        urlParam.put("idCandidate", 1L);
+        ResponseEntity<Double> rep = testRestTemplate.exchange("/api/candidates/{idCandidate}/average", HttpMethod.GET, new HttpEntity<>(null, headers), Double.class,urlParam);
+        assertThat(rep.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void getCandidateAverageThrow(){
+        final HttpHeaders headers = new HttpHeaders();
+        final HashMap<String, Object> urlParam = new HashMap<>();
+        urlParam.put("idCandidate", "Le candidat pas été trouvé");
+        ResponseEntity<ChangeSetPersister.NotFoundException> rep = testRestTemplate.exchange("/api/candidates/{idCandidate}", HttpMethod.GET, new HttpEntity<>(null, headers), ChangeSetPersister.NotFoundException.class, urlParam);
+        assertThat(rep.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 
 }
